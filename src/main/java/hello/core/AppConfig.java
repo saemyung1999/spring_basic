201de184427a7403d7879bc4c0bb2f44665a0b64
saemyung1999 <saemyung1999@gmail.com>
2020-11-16 21:27:02 +0900

    Spring 컨테이너(싱글톤 컨테이너)와 @Configuration에 대한 테스트 작성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,6 +18,7 @@ public class AppConfig {
     //이후에 다른 구현클래스로 변경 시에 return문의 클래스명만 변경해주면 됨
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
@@ -29,11 +30,13 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
