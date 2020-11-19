225fd77c89c647c8c32b672944a057584ab31943
saemyung1999 <saemyung1999@gmail.com>
2020-11-19 20:28:49 +0900

    의존관계 자동 주입 - 등록한 빈 모두 조회 및 빈 수동/자동 등록의 여부 테스트

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -33,7 +33,7 @@ public class OrderServiceImpl implements OrderService {
 
     //생성자를 통한 의존성 주입 -> 생성자가 하나일 경우에는 @Autowired를 붙이지 않아도 됨 ==> 생성자 의존성 주입을 사용하자!!!!
 //    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) { //동일한 타입의 빈이 2개 이상일 경우 파라미터명으로 빈 이름 매칭하여 데려옴
+//    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) { //동일한 타입의 빈이 2개 이상일 경우 파라미터명으로 빈 이름 매칭하여 데려옴
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
---
+++ src/test/java/hello/core/autowired/allbean/AllBeanTest.java
@@ -0,0 +1,52 @@
+package hello.core.autowired.allbean;
+
+import hello.core.AppConfig;
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Getter
+    @Setter
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+}
